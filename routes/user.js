60ad24f5cbb6ceaa44aa732557c76c25bd9b62f0
Rameez <rameez007rz@gmail.com>
2021-04-29 01:13:53 +0530

    Feature Added : Cart & Add-to-cart function also

--- routes/user.js
+++ routes/user.js
@@ -3,61 +3,74 @@ var router = express.Router();
 const productHelpers = require('../helpers/product-helpers');
 const userHelpers = require('../helpers/user-helpers');
 const { response } = require('../app');
-const verifyLogin=(req,res,next)=>{
-  if(req.session.loggedIn){
+const verifyLogin = (req, res, next) => {
+  if (req.session.loggedIn) {
     next()
-  }else{
+  } else {
     res.redirect('/login')
   }
-  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  let user=req.session.user
+router.get('/', async function (req, res, next) {
+  let user = req.session.user
   console.log(user)
-  productHelpers.getAllProducts().then((products)=>{
-		
-		res.render('user/view-products', {products,user})
-	})
+  let cartCount=null
+  if(req.session.user){
+  cartCount=await userHelpers.getCartCount(req.session.user._id)
+  }
+  productHelpers.getAllProducts().then((products) => {
+
+    res.render('user/view-products', { products, user,cartCount })
+  })
 });
 
-router.get('/login',(req,res)=>{
-  if(req.session.loggedIn){
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
     res.redirect('/')
-  }else{
-    res.render('user/login',{"loginErr":req.session.loginErr})
-    req.session.loginErr=false
+  } else {
+    res.render('user/login', { "loginErr": req.session.loginErr })
+    req.session.loginErr = false
   }
 })
 
-router.get('/signup',(req,res)=>{
+router.get('/signup', (req, res) => {
   res.render('user/signup')
 })
-router.post('/signup',(req,res)=>{
-  userHelpers.doSignup(req.body).then((response)=>{
+router.post('/signup', (req, res) => {
+  userHelpers.doSignup(req.body).then((response) => {
     console.log(response);
-    
+    req.session.loggedIn = true
+    req.session.user = response
+    res.redirect('/')
   })
 })
-router.post('/login',(req,res)=>{
-  userHelpers.doLogin(req.body).then((response)=>{
-    if(response.status){
-      req.session.loggedIn=true
-      req.session.user=response.user
+router.post('/login', (req, res) => {
+  userHelpers.doLogin(req.body).then((response) => {
+    if (response.status) {
+      req.session.loggedIn = true
+      req.session.user = response.user
       res.redirect('/')
-    }else{
-      req.session.loginErr="Invalid Username or Password"
+    } else {
+      req.session.loginErr = "Invalid Username or Password"
       res.redirect('/login')
     }
   })
-  })
-router.get('/logout',(req,res)=>{
+})
+router.get('/logout', (req, res) => {
   req.session.destroy()
   res.redirect('/')
 })
-router.get('/cart',verifyLogin, (req,res)=>{
-  res.render('user/cart')
+router.get('/cart', verifyLogin, async(req, res) => {
+  let products=await userHelpers.getCartProducts(req.session.user._id)
+  console.log(products);
+  res.render('user/cart',{products,user:req.session.user._id})
 })
-
+router.get('/add-to-cart/:id', (req, res) => {
+  console.log("api call");
+  userHelpers.addToCart(req.params.id, req.session.user._id).then(() => {
+    res.json({status:true})
+  })
+})
 
 module.exports = router;
